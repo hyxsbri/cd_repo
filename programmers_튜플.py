4a43b14552cbdf5a9b791c5c3d41f50017fb1f0c
HYOWON CHOI <97431260+hyxsbri@users.noreply.github.com>
2022-10-07 16:33:30 +0900

    Add files via upload

---
+++ programmers_튜플.py
@@ -0,0 +1,11 @@
+def solution(s):
+    ans = []
+    s = s[2: -2]
+    s = s.split('},{')
+    s.sort(key = len)
+    for i in s:
+        x = i.split(',')
+        for j in x:
+            if int(j) not in ans:
+                ans.append(int(j))
+    return ans
