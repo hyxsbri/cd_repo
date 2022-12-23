e6c0e8500af0a229931be6d4fe3e203a92fbef02
Hyowon Choi <97431260+hyxsbri@users.noreply.github.com>
2022-12-23 17:14:14 +0900

    Add files via upload

---
+++ boj/boj_대표값2.py
@@ -0,0 +1,40 @@
+
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+l = [0]*10000001
+for _ in range(n):
+    l[int(input())] += 1
+
+for i in range(10000001):
+    if l[i] != 0:
+        for j in range(l[i]):
+            print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ boj/boj_수 정렬하기 3.py
@@ -0,0 +1,53 @@
+
+
+import sys
+input = sys.stdin.readline
+from collections import Counter
+n = int(input())
+l = []
+
+for _ in range(n):
+    l.append(int(input()))
+l.sort()
+l_mod = Counter(l).most_common()
+print(round(sum(l)/n))
+print(l[n//2])
+if len(l_mod) > 1:
+    if l_mod[0][1] == l_mod[1][1]:
+        print(l_mod[1][0])
+    else:
+        print(l_mod[0][0])
+else:
+    print(l_mod[0][0])
+print(l[-1]-l[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
