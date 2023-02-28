f3fd55c99fe28bd71c80f55aeaf469299ef84988
Hyowon Choi / 최효원 <gydnjs1231@yonsei.ac.kr>
2023-02-28 18:47:03 +0900

    Add files via upload

--- boj/자료구조/boj_괄호.py
+++ boj/자료구조/boj_괄호.py
@@ -1,12 +1,24 @@
 
 
-from collections import deque
-
-dq = deque()
-dq.append(123)
-dq.append(456)
-dq.append(789)
-
-while len(dq):
-    print(dq.popleft())
+# 스택 - FILO / LIFO
+for _ in range(int(input())):
+    stk = []
+    ans = 'YES'
+    for c in input():
+        if c == '(':
+            stk.append(c)
+            # 여는 괄호는 스택에 삽입
+        else:
+            # 닫는 괄호일 시,
+            if len(stk)>0:
+                stk.pop()
+                # 여는 괄호 남아있으면 스택에서 삭제
+            else:
+                ans = 'NO'
+                # 남아있지 않으면 NO
+    if len(stk)>0:
+        ans = 'NO'
+        # 모든 작업 완료 후 여는 괄호 남아있으면 NO
+    
+    print(ans)
 
